b5fa277046c15cd803fef05c9675a15239ba5853
WildBarreto <wildsbsrreto@gmail.com>
2024-02-01 21:44:02 -0300

    Primeiro component Menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import './App.css'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import "./App.css";
+import capa from "./assets/capa-a-playstation-store-psn-finalmente-implementa-um-recurso-altamente-solicitado.jpg";
+import logo from "./assets/ps-logo.png";
+import Menu from "./components/menu/Menu";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <img src={logo} alt="" className="logo" />
+      <Menu />
+      <img src={capa} alt="" className="capa" />
+      <h1>Ofertas</h1>
+      <aside>
+        <h2>Informações Adicionais</h2>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Este é o espaço para informações adicionais sobre o catalogo de jogos,
+          promoções, noticias, etc.
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </aside>
+    
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,20 @@
+import "./menu.css";
+//import { robotoRegular, robotoBold } from './fonts';
+
+
+function Menu() {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>Novidades</li>
+          <li>Ofertas</li>
+          <li>Sobre</li>
+          <li>Newsletter</li>
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+export default Menu;
